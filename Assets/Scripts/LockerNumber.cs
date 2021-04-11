a258dacabdbf0b481d9edd61a69f9414527e6bd7
Chahat Kalsi <chahat.ck88@gmail.com>
2021-04-11 11:58:13 +0530

    locker mechanism added

---
+++ Assets/Scripts/LockerNumber.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerNumber : MonoBehaviour, IInteractable
+{
+    public void Interact(DisplayImage currentDisplay)
+    {
+        transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()]++;
+        // index of sibling 1 = 0, last = 4
+        // so we are increasing the number by one on each click
+
+        if (transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()] > 9)
+            transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()] = 0;
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite =
+            transform.parent.GetComponent<NumberLock>().numberSprites[transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()]];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NumberLock.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberLock : MonoBehaviour
+{
+    public string Password;
+
+    public GameObject OpenLockerSprite;
+
+    [HideInInspector]
+    public Sprite[] numberSprites;
+    [HideInInspector]
+    public int[] currentIndividualIndex = { 0, 0, 0, 0, 0 };
+    // current index is the current password
+
+    private bool isOpen;
+
+    private GameObject displayImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayImage = GameObject.Find("displayImage");
+
+        OpenLockerSprite.SetActive(false);
+        LoadAllNumberSprites();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenLocker();
+        LayerManager();
+    }
+
+    void LoadAllNumberSprites()
+    {
+        numberSprites = Resources.LoadAll<Sprite>("Sprites/numbers");
+    }
+
+    bool VerifyCorrectCode()
+    {
+        bool correct = true;
+
+        for(int i=0; i<5; ++i)
+        {
+            if (Password[i] != transform.GetChild(i).GetComponent<SpriteRenderer>().sprite.name[transform.GetChild(i).GetComponent<SpriteRenderer>().sprite.name.Length - 1])
+            {
+                correct = false;
+            }
+        }
+
+        return correct;
+    }
+
+    void OpenLocker()
+    {
+        if (isOpen) return;
+
+        if (VerifyCorrectCode())
+        {
+            isOpen = true;
+            // Debug.Log("Password correct!");
+            OpenLockerSprite.SetActive(true);
+
+            for(int i=0; i<5; ++i)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    void LayerManager()
+    {
+        if (isOpen) return;
+
+        if(displayImage.GetComponent<DisplayImage>().CurrentState == DisplayImage.State.normal)
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.layer = 2;
+                // layer 2 is ignore raycast. so it esentially turns of the interact function of numbers in normal state
+                // we only need interact in zoomed in state.
+            }
+        }
+        else
+        {
+            foreach (Transform child in transform) 
+            {
+                child.gameObject.layer = 0;
+            }
+                    
+        }
+    }
+}
