2f4a31bbbb236000d646cca125c1d3ce1c9d8fa8
Chahat Kalsi <chahat.ck88@gmail.com>
2021-04-11 12:34:47 +0530

    cupboard opening key mechanism added

--- Assets/Scripts/Cupboard.cs
+++ Assets/Scripts/Cupboard.cs
@@ -10,12 +10,15 @@ public class Cupboard : MonoBehaviour, IInteractable
 
     private GameObject inventory;
 
+    private GameObject cupboardRight;
+
     public void Interact(DisplayImage currentDisplay)
     {
         // if the current selected slot has an item whose name is the same as UnlockItem, then
         if (inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem)
         {
             Debug.Log("unlock");
+            cupboardRight.SetActive(true);
             inventory.GetComponent<Inventory>().currentSelectedSlot.GetComponent<Slot>().ItemProperty = Slot.property.empty;
             inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite =
                 Resources.Load<Sprite>("Items/empty");
@@ -27,6 +30,8 @@ public class Cupboard : MonoBehaviour, IInteractable
     void Start()
     {
         inventory = GameObject.Find("Inventory");
+        cupboardRight = GameObject.Find("cupboardRight");
+        cupboardRight.SetActive(false);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/pickAfterZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickAfterZoom : MonoBehaviour
+{
+    private GameObject displayImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayImage = GameObject.Find("displayImage");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LayerManager();
+    }
+
+    void LayerManager()
+    {
+        if (displayImage.GetComponent<DisplayImage>().CurrentState == DisplayImage.State.normal)
+        {
+            gameObject.layer = 2;
+        }
+        else gameObject.layer = 0;
+    }
+}
